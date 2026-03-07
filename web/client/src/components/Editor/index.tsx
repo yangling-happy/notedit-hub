2209eaaea38a60b428c51e6c510e005633830449
Amber Yang <yamber0302@qq.com>
2026-03-07 19:35:19 +0800

    feat(editor):supports Chinese

--- web/client/src/components/Editor/index.tsx
+++ web/client/src/components/Editor/index.tsx
@@ -4,8 +4,9 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import "@blocknote/core/fonts/inter.css";
-
+import { zh } from "@blocknote/core/locales";
+  
 export default function Editor() {
-  const editor = useCreateBlockNote();
+  const editor = useCreateBlockNote({ dictionary: zh,});
   return <BlockNoteView editor={editor}/>;
 }
