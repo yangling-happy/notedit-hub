85c9bc58f1d0c86f2509eaf5d1593b5709c4bdc4
Amber Yang <yamber0302@qq.com>
2026-03-08 13:53:50 +0800

    chore: remove toolbar icon

--- web/client/src/components/Toolbar/index.tsx
+++ web/client/src/components/Toolbar/index.tsx
@@ -1,10 +1,3 @@
-import { Button, Space } from "antd";
-import {
-  FileOutlined,
-  FolderOpenOutlined,
-  SaveOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
 
 export const Toolbar = () => (
   <div
@@ -21,20 +14,7 @@ export const Toolbar = () => (
       zIndex: 2,
     }}
   >
-    <Space>
-      <Button type="text" icon={<FileOutlined />}>
-        新建
-      </Button>
-      <Button type="text" icon={<FolderOpenOutlined />}>
-        打开
-      </Button>
-      <Button type="text" icon={<SaveOutlined />}>
-        保存
-      </Button>
-      <Button type="text" icon={<SettingOutlined />}>
-        设置
-      </Button>
-    </Space>
+    
     <div style={{ fontWeight: 600, fontSize: 16, color: "#1677ff" }}>
       Notedit 此处是顶部工具栏
     </div>
