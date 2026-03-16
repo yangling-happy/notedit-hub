534f02be8d2e363282305bfd1db0bc7ca690ad9e
Amber Yang <yamber0302@qq.com>
2026-03-16 13:42:26 +0800

    feat(i18n): add language persistence with LanguageDetector, default to zh-CN

--- web/client/src/locales/i18.ts
+++ web/client/src/locales/i18.ts
@@ -1,28 +1,31 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./en.json";
 import zh from "./zh.json";
 
-
 const resources = {
   en: {
-    translation: en  
+    translation: en,
   },
   zh: {
-    translation: zh
-  }
-};
-i18n.use(initReactI18next).init({
-  resources,
-  fallbackLng: "zh",
-  detection: {
-      order: ['localStorage', 'navigator'], 
-      caches: ['localStorage'], 
-    },
-  interpolation: {
-    escapeValue: false, // react already safes from xss
+    translation: zh,
   },
-});
+};
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "zh",
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
 
 export default i18n;
